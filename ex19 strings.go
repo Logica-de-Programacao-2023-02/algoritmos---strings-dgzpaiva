0883b6509f7493c847478032c9e89dc7bb104cbb
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-04 00:38:34 -0300

    Update ex19 strings.go

--- ex19 strings.go
+++ ex19 strings.go
@@ -1,1 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var input string
+
+	fmt.Print("Digite uma string: ")
+	fmt.Scanln(&input)
+
+	fmt.Println("String invertida:", inverterString(input))
+}
+
+func inverterString(str string) string {
+	runes := []rune(str)
+	tamanho := len(runes)
+
+	for i := 0; i < tamanho/2; i++ {
+		runes[i], runes[tamanho-1-i] = runes[tamanho-1-i], runes[i]
+	}
+
+	return string(runes)
+}
 
