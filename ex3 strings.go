d26a66f866f1cf30b361e5dc1989740e93c0cecc
dgzpaiva <143409527+dgzpaiva@users.noreply.github.com>
2023-12-04 00:21:00 -0300

    Create ex3 strings.go

---
+++ ex3 strings.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var input string
+	var antigo, novo rune
+
+	fmt.Print("Digite uma string: ")
+	fmt.Scanln(&input)
+
+	fmt.Print("Digite o caractere a ser substituído: ")
+	fmt.Scanf("%c", &antigo)
+
+	fmt.Print("Digite o novo caractere: ")
+	fmt.Scanf(" %c", &novo)
+	resultado := substituirCaractere(input, antigo, novo)
+	fmt.Println("String com substituição:", resultado)
+}
+
+func substituirCaractere(str string, antigo, novo rune) string {
+	runes := []rune(str)
+
+	for i, char := range runes {
+		if char == antigo {
+			runes[i] = novo
+		}
+	}
+
+	return string(runes)
+}
